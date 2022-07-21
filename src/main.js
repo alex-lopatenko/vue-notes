efb271f32df858e433b5764b21ddf4269075d7e8
Alexey <alexey-lopatenko@yandex.ru>
2022-07-21 09:15:03 +0300

    Connected vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
-
 import routers from './routes'
-
+import { store } from './store'
 import App from './App.vue'
 
 import './assets/scss/main.scss'
-
 const app = createApp(App)
 
 app.use(routers)
+app.use(store)
 
 app.mount('#app')
