fb840a809c9db99da5b5bbecfae5241d1def8763
Alexey <alexey-lopatenko@yandex.ru>
2022-07-24 09:47:28 +0300

    Add state and getters in createStore

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,24 @@
 <template>
-  <p>vuex</p>
+  <ul>
+    <li v-for="user in getUsers" :key="user.id">
+      <p>{{user.id}}</p>
+      <p>{{user.name}}</p>
+      <p>{{user.admin ? 'Это админ' : 'Обычный юзер'}}</p>
+    </li>
+  </ul>
+
+  <p>{{ getUsersLength }}</p>
 </template>
 
 <script>
 export default {
-  
+  computed: {
+    getUsers () {
+      return this.$store.getters.getUsers
+    },
+    getUsersLength() {
+      return this.$store.getters.getUsersLength
+    }
+  }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,20 @@
 import {createStore} from 'vuex'
 
 export const store = createStore({
-    //
+    state: {
+        users: [
+            { id: 1, name: 'Eugeniy', admin: true},
+            { id: 2, name: 'Alex', admin: true},
+            { id: 3, name: 'Diego', admin: false}
+        ]
+    },
+    getters: {
+        getUsers(state) {
+            return state.users
+        },
+        getUsersLength(state, getters) {
+            const suffix = 'Кол-во пользователей:'
+            return `${suffix}  ${getters.getUsers.length}`
+        }
+    }
 })
