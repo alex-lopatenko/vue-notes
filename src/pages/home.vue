c879eecb862624770637b57b8f934962d347f988
Alexey <alexey-lopatenko@yandex.ru>
2022-07-29 12:56:54 +0300

    Add actions and mutations

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,42 +1,66 @@
 <template>
-<h1>Только обычные пользователи:</h1>
-  <ul>
-    <li v-for="user in getUsers" :key="user.id">
-      <p>{{user.id}}</p>
-      <p>{{user.name}}</p>
-      <p>{{user.admin ? 'Это админ' : 'Обычный юзер'}}</p>
-    </li>
-  </ul>
+  <section>
+    <button
+      v-if="isUserLogged"
+      @click="handleLoginClick"
+      class="btn btnPrimary"
+    >
+      Login
+    </button>
+    <p v-else>
+      logged user: <i>{{ user }}</i>
+    </p>
+  </section>
 
-  <br />
-  <br />
+  <section>
+    <h1>Только обычные пользователи:</h1>
+    <ul>
+      <li v-for="user in getUsers" :key="user.id">
+        <p>{{ user.id }}</p>
+        <p>{{ user.name }}</p>
+        <p>{{ user.admin ? "Это админ" : "Обычный юзер" }}</p>
+      </li>
+    </ul>
+  </section>
 
-<input v-model="userId" type="number" />
-<p>{{ getUser }}</p>
+  <section>
+    <input v-model="userId" type="number" />
+    <p>{{ getUser }}</p>
+  </section>
 
-    <br />
-    <br />
-
-  <p>{{ getUsersLength }}</p>
+  <section>
+    <p>{{ getUsersLength }}</p>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      userId: 3,
-    }
+      userId: 1,
+    };
+  },
+  methods: {
+    handleLoginClick() {
+      this.$store.dispatch("setUser");
+    },
   },
   computed: {
-    getUser () {
-      return this.$store.getters.getUserById(this.userId) || 'user not found'
+    isUserLogged() {
+      return this.$store.getters.isUserLogged;
     },
-    getUsers () {
-      return this.$store.getters.getUsers
+    user() {
+      return this.$store.getters.getUser;
+    },
+    getUser() {
+      return this.$store.getters.getUserById(this.userId) || "user not found";
+    },
+    getUsers() {
+      return this.$store.getters.getUsers;
     },
     getUsersLength() {
-      return this.$store.getters.getUsersLength
-    }
-  }
-}
+      return this.$store.getters.getUsersLength;
+    },
+  },
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,39 @@ import {createStore} from 'vuex'
 
 export const store = createStore({
     state: {
+        user: null,
         users: [
             { id: 1, name: 'Eugeniy', admin: true},
             { id: 2, name: 'Alex', admin: true},
             { id: 3, name: 'Diego', admin: false}
         ]
     },
+
+    mutations: {
+        setUser (state, user) {
+            state.user = user
+        }
+    },
+    actions: {
+        setUser ({ commit }) {
+            setTimeout(() => {
+                const fakeUser = {
+                id: 1239214,
+                name: 'Diego',
+                admin: false
+              }
+              commit('setUser', fakeUser)
+            }, 1000)
+        }
+    },
+
     getters: {
+        isUserLogged (state) {
+            return state.user ? true : false
+        },
+        getUser (state) {
+            return state.user
+        },
         getAllUsers(state) {
             return state.users
         },
